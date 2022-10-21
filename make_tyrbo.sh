c79d01cbdc4ce8ba64fbf84f1b332e7ff3f907cc
procub3r <procub3r@protonmail.com>
2022-10-21 22:56:45 +0530

    Add make clean rule. Update ./make_tyrbo.sh script

--- make_tyrbo.sh
+++ make_tyrbo.sh
@@ -1,3 +1,13 @@
-echo compiling tyrbo...
-make -f mktyrbo.mk
-echo to run tyrbo: ./tyrbo
+if [[ $1 == 'compile' ]]; then
+    echo compiling tyrbo...
+    make -f mktyrbo.mk
+    echo to run tyrbo: ./tyrbo
+elif [[ $1 == 'clean' ]]; then
+    echo cleaning intermediate and target files...
+    make -f mktyrbo.mk clean
+    echo done!
+else
+    echo ./make_tyrbo.sh usage:
+    echo ./make_tyrbo.sh compile \# to compile tyrbo
+    echo ./make_tyrbo.sh clean \# to clean output files
+fi
