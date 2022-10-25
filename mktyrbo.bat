d64e7442708ef0bfc4373242df7d402d14e74095
Harsh Somvanshi <harsh.som33@gmail.com>
2022-10-25 17:40:10 +0530

    Update mktyrbo.bat

--- mktyrbo.bat
+++ mktyrbo.bat
@@ -1,8 +1,7 @@
 @echo off 
 
 set arg1=%1
-@REM if [%arg1%]==[] goto make else if %arg1%==clean del *.o tyrbo.exe
-@REM if [%arg1%]==[] del *.o tyrbo.exe
+
 if [%arg1%]==[] (
     mingw32-make -f resources/lib/mktyrbo.mk 
     goto commonexit
