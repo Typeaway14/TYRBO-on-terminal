5c584d000295e7c2100471179e156d2717575a9e
Typeaway14 <harsh.som33@gmail.com>
2022-11-09 23:42:42 +0530

    Final file structure on Linux and Windows

--- mktyrbo.sh
+++ mktyrbo.sh
@@ -4,7 +4,7 @@ if [[ $1 == '' ]]; then
     echo to run tyrbo: ./tyrbo
 elif [[ $1 == 'clean' ]]; then
     echo cleaning intermediate and target files...
-    rm resources/lib/*.o tyrbo
+    rm resources/bin/*.o tyrbo
     echo done!
 else
     echo Usage:
