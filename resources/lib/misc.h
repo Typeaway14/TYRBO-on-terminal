663cabeb5735814562f4f8868b689701742ea3ec
Harsh Somvanshi <harsh.som33@gmail.com>
2022-11-30 22:30:13 +0530

    Update misc.h

--- resources/lib/misc.h
+++ resources/lib/misc.h
@@ -7,6 +7,7 @@ void trimTrailing(char * str);
 int bball_dunk();
 int handle_wrong_case(FILE* fp,int* b,int* streak,int* count,CHAR_NODE **node,int x,int y,int size,int color);
 void score_save(int gmode,float *wpm,float *acc,float *netwpm,int BBscore);
+void score(float time_taken,int count,int size,char gmode,int BBscore);
 void caps_check();
 void problem_keys_analysis();
 void clean_and_exit();
