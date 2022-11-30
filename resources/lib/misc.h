8f360eab156a1bc60b36018e3fe4f19f7b04db87
Kiran-raj <santoshrajpurohit89@gmail.com>
2022-11-30 21:26:22 +0530

    remove stupid "documentation"

--- resources/lib/misc.h
+++ resources/lib/misc.h
@@ -4,7 +4,6 @@
 #include"type_structures.h"
 
 void trimTrailing(char * str);
-void score(float,int,int,char,int);//function to calculate the user score.Parameters:(time_taken,total characters input by user,size of string)
 int bball_dunk();
 int handle_wrong_case(FILE* fp,int* b,int* streak,int* count,CHAR_NODE **node,int x,int y,int size,int color);
 void score_save(int gmode,float *wpm,float *acc,float *netwpm,int BBscore);
