a7c5ae3c01f6a72d0774bc1bcae3dbda2565f782
Typeaway14 <harsh.som33@gmail.com>
2022-12-03 17:21:23 +0530

    optimized makefile

--- resources/lib/mktyrbo.mk
+++ resources/lib/mktyrbo.mk
@@ -1,16 +1,8 @@
-tyrbo.exe: type_structures.o type_content.o client.o tc.o art.o misc.o signal_handlers.o
-	gcc -g resources/bin/client.o resources/bin/type_content.o resources/bin/misc.o resources/bin/art.o resources/bin/type_structures.o resources/bin/tc.o resources/bin/signal_handlers.o -o tyrbo
-type_content.o: resources/src/type_content.c
-	gcc -c -g resources/src/type_content.c -o resources/bin/type_content.o
-client.o: resources/src/client.c
-	gcc -c -g resources/src/client.c -o resources/bin/client.o
-tc.o: resources/src/tc.c
-	gcc -c -g resources/src/tc.c -o resources/bin/tc.o
-type_structures.o: resources/src/type_structures.c
-	gcc -c -g resources/src/type_structures.c -o resources/bin/type_structures.o
-art.o: resources/src/art.c
-	gcc -g -c resources/src/art.c -o resources/bin/art.o
-misc.o: resources/src/misc.c
-	gcc -c -g resources/src/misc.c -o resources/bin/misc.o
-signal_handlers.o: resources/src/signal_handlers.c
-	gcc -c -g resources/src/signal_handlers.c -o resources/bin/signal_handlers.o
+CC := gcc
+CFLAGS := -g
+obj_files := type_structures.o type_content.o client.o tc.o art.o misc.o signal_handlers.o
+
+tyrbo.exe: ${obj_files}
+	${CC} ${CFLAGS} ${addprefix resources/bin/,${obj_files}} -o tyrbo
+%.o: resources/src/%.c
+	${CC} -c ${CFLAGS} ${<} -o resources/bin/${@}
